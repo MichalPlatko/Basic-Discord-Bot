d05d360798d65a161c5bb8ddf9f899669c1f5be1
MichalPlatko <miso.platko@gmail.com>
2020-02-11 13:12:38 +0100

    added changes

--- commands/info/features.js
+++ commands/info/features.js
@@ -1,12 +1,12 @@
 module.exports = {
     name: "feature",
-    aliases:[""],
+    aliases:["issue"],
     category : "info",
     description : "Logs feature-ideas messages",
     run : async (client,message,args)=>{
         const channel = message.guild.channels.find(channel => channel.name ==="feature-ideas");
         const msgcontent=message.content.split("!feature");
-        const msg=`${message.author.username} reported feature : ${msgcontent[1]}`;
+        const msg=`${message.author.username} thought of feature : ${msgcontent[1]}`;
         channel.send(msg);                   
     }              
 }
